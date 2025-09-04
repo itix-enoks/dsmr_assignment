1f4499352787bb874614563609b43818d1c47833
enoks <enoks@tutanota.com>
2025-09-04 23:17:06 +0200

    Write a baseline for constructing and validating telegram contents

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use error::MainError;
+use dsmr_assignment::error::MainError;
 use std::io::Read;
-use tudelft_dsmr_output_generator::voltage_over_time::{
-    create_voltage_over_time_graph, VoltageData,
-};
-use tudelft_dsmr_output_generator::Graphs;
+// use tudelft_dsmr_output_generator::voltage_over_time::{
+//     create_voltage_over_time_graph, VoltageData,
+// };
+// use tudelft_dsmr_output_generator::Graphs;
 
 /// Contains `MainError`, and code to convert `PlotError` and `io::Error` into a `MainError`
-mod error;
+// Package declaration moved to lib.rs
+// // mod error;
 
-fn parse(_input: &str) -> Result<(), MainError> {
-    // Note that you can use this function:
-    // tudelft_dsmr_output_generator::date_to_timestamp()
-
-    todo!()
-}
+// Moved my parser to different module for own sake.
+use dsmr_assignment::parser::parse;
 
 /// Reads the DSMR file from the terminal.
 /// You do not need to change this nor understand this.
@@ -34,24 +31,24 @@ fn read_from_stdin() -> Result<String, MainError> {
 fn main() -> Result<(), MainError> {
     let input = read_from_stdin()?;
 
-    let _parsed = parse(&input)?;
+    let _ = parse(&input)?;
 
-    let mut result = Graphs::new()?;
+    // let mut result = Graphs::new()?;
 
-    result.add_graph(create_voltage_over_time_graph(vec![
-        VoltageData {
-            phase_1: 100.0,
-            phase_2: 200.0,
-            phase_3: 300.0,
-            timestamp: 100,
-        },
-        VoltageData {
-            phase_1: 200.0,
-            phase_2: 300.0,
-            phase_3: 250.0,
-            timestamp: 10000,
-        },
-    ]))?;
+    // result.add_graph(create_voltage_over_time_graph(vec![
+    //     VoltageData {
+    //         phase_1: 100.0,
+    //         phase_2: 200.0,
+    //         phase_3: 300.0,
+    //         timestamp: 100,
+    //     },
+    //     VoltageData {
+    //         phase_1: 200.0,
+    //         phase_2: 300.0,
+    //         phase_3: 250.0,
+    //         timestamp: 10000,
+    //     },
+    // ]))?;
 
     Ok(())
 }
