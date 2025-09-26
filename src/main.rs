14dda0eef54873ca970f93d29b8a014d7e938c5e
enoks <enoks@tutanota.com>
2025-09-26 11:06:18 +0200

    Fix currents, voltages and gas plots not being plotted by their m...
    Fix currents, voltages and gas plots not being plotted by their max
    value.
    
    Fix energy_over_time_plot's not being aggregated at all.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io::Read;
 
 use dsmr_assignment::bail;
+use itertools::Itertools;
 
 use tudelft_dsmr_output_generator::voltage_over_time::{ create_voltage_over_time_graph, VoltageData };
 use tudelft_dsmr_output_generator::current_over_time::{ CurrentOverTime, CurrentData };
@@ -62,7 +63,7 @@ fn main() -> Result<(), MainError> {
     // Duplicate timestamps in dsmr file messes up the energy_over_time plot, it does not seem to
     //  mess up the other plots though, but I still skip duplicate timestamps in the other plots
     //  for now anyway.
-    let mut processed_timestamps: HashSet<UnixTimeStamp> = HashSet::new();
+    let mut processed_map: HashMap<UnixTimeStamp, (f64, f64, f64)> = HashMap::new();
     let voltages: Vec<VoltageData> =
         telegrams
         .iter()
@@ -71,22 +72,39 @@ fn main() -> Result<(), MainError> {
                 Some(Value::Date(date)) => date.timestamp,
                 _ => bail!("Invalid timestamp")
             };
-            if processed_timestamps.contains(&timestamp) {
-                return Option::None
-            }
             match &t.data {
                 TelegramData::Electricity { voltages, .. }  =>
                     match voltages {
                         [TelegramContent { value: Some(Value::Float(p1)), .. },
                          TelegramContent { value: Some(Value::Float(p2)), .. },
                          TelegramContent { value: Some(Value::Float(p3)), .. }] => {
-                            processed_timestamps.insert(timestamp);
-                            Some(VoltageData {
-                                timestamp: timestamp,
-                                phase_1: *p1,
-                                phase_2: *p2,
-                                phase_3: *p3
-                            })
+                            if !processed_map.contains_key(&timestamp) {
+                                processed_map.insert(timestamp, (*p1, *p2, *p3));
+                                Some(VoltageData {
+                                    timestamp: timestamp,
+                                    phase_1: *p1,
+                                    phase_2: *p2,
+                                    phase_3: *p3
+                                })
+                            } else {
+                                let mut new_value = processed_map.get(&timestamp).unwrap_or_else(|| bail!("Could not unpack old_value")).clone();
+                                if *p1 > new_value.0 {
+                                    new_value.0 = *p1;
+                                }
+                                if *p2 > new_value.1 {
+                                    new_value.1 = *p2;
+                                }
+                                if *p3 > new_value.2 {
+                                    new_value.2 = *p3;
+                                }
+                                processed_map.insert(timestamp, new_value);
+                                Some(VoltageData {
+                                    timestamp: timestamp,
+                                    phase_1: new_value.0,
+                                    phase_2: new_value.1,
+                                    phase_3: new_value.2
+                                })
+                            }
                         },
                         _ => Option::None
                     },
@@ -95,7 +113,7 @@ fn main() -> Result<(), MainError> {
         })
         .collect();
 
-    let mut processed_timestamps: HashSet<UnixTimeStamp> = HashSet::new();
+    let mut processed_map: HashMap<UnixTimeStamp, (f64, f64, f64)> = HashMap::new();
     let currents: Vec<CurrentData> =
         telegrams
         .iter()
@@ -104,22 +122,39 @@ fn main() -> Result<(), MainError> {
                 Some(Value::Date(date)) => date.timestamp,
                 _ => bail!("Invalid timestamp")
             };
-            if processed_timestamps.contains(&timestamp) {
-                return Option::None
-            }
             match &t.data {
                 TelegramData::Electricity { currents, .. }  =>
                     match currents {
                         [TelegramContent { value: Some(Value::Float(p1)), .. },
                          TelegramContent { value: Some(Value::Float(p2)), .. },
                          TelegramContent { value: Some(Value::Float(p3)), .. }] => {
-                            processed_timestamps.insert(timestamp);
-                            Some(CurrentData {
-                                timestamp: timestamp,
-                                phase_1: *p1,
-                                phase_2: *p2,
-                                phase_3: *p3
-                            })
+                            if !processed_map.contains_key(&timestamp) {
+                                processed_map.insert(timestamp, (*p1, *p2, *p3));
+                                Some(CurrentData {
+                                    timestamp: timestamp,
+                                    phase_1: *p1,
+                                    phase_2: *p2,
+                                    phase_3: *p3
+                                })
+                            } else {
+                                let mut new_value = processed_map.get(&timestamp).unwrap_or_else(|| bail!("Could not unpack old_value")).clone();
+                                if *p1 > new_value.0 {
+                                    new_value.0 = *p1;
+                                }
+                                if *p2 > new_value.1 {
+                                    new_value.1 = *p2;
+                                }
+                                if *p3 > new_value.2 {
+                                    new_value.2 = *p3;
+                                }
+                                processed_map.insert(timestamp, new_value);
+                                Some(CurrentData {
+                                    timestamp: timestamp,
+                                    phase_1: new_value.0,
+                                    phase_2: new_value.1,
+                                    phase_3: new_value.2
+                                })
+                            }
                         },
                         _ => Option::None
                     },
@@ -132,7 +167,7 @@ fn main() -> Result<(), MainError> {
         current_over_time.add(c);
     }
 
-    let mut processed_timestamps: HashSet<UnixTimeStamp> = HashSet::new();
+    let mut processed_map: HashMap<UnixTimeStamp, f64> = HashMap::new();
     let gas_pairs: Vec<(UnixTimeStamp, f64)> =
         telegrams
         .iter()
@@ -141,15 +176,23 @@ fn main() -> Result<(), MainError> {
                 Some(Value::Date(date)) => date.timestamp,
                 _ => bail!("Invalid timestamp")
             };
-            if processed_timestamps.contains(&timestamp) {
-                return Option::None
-            }
-            match &t.data {
+            return match &t.data {
                 TelegramData::Gas { total_gas_delivered }  =>
                     match total_gas_delivered {
                         TelegramContent { value: Some(Value::Float(gas)), .. } => {
-                            processed_timestamps.insert(timestamp);
-                            Some((timestamp, *gas))
+                            if !processed_map.contains_key(&timestamp) {
+                                processed_map.insert(timestamp, *gas);
+                                Some((timestamp, *gas))
+                            } else {
+                                let new_value = processed_map.get(&timestamp).unwrap_or_else(|| bail!("Could not unpack old_value")).clone();
+                                if new_value > *gas {
+                                    processed_map.insert(timestamp, new_value);
+                                    Some((timestamp, new_value))
+                                } else {
+                                    processed_map.insert(timestamp, *gas);
+                                    Some((timestamp, *gas))
+                                }
+                            }
                         },
                         _ => Option::None
                     },
@@ -168,10 +211,11 @@ fn main() -> Result<(), MainError> {
         });
     }
 
-    let total_pairs: Vec<(UnixTimeStamp, (f64, f64))> =
+    let mut processed_map: HashMap<UnixTimeStamp, (f64, f64)> = HashMap::new();
+    let _aggregate =
         telegrams
         .iter()
-        .filter_map(|t| {
+        .for_each(|t| {
             let timestamp = match &t.base.date.value {
                 Some(Value::Date(date)) => date.timestamp,
                 _ => bail!("Invalid timestamp")
@@ -180,23 +224,25 @@ fn main() -> Result<(), MainError> {
                 TelegramData::Electricity { total_consumed, total_produced, .. }  =>
                     match (total_consumed, total_produced) {
                         (TelegramContent { value: Some(Value::Float(consumed)), .. },
-                         TelegramContent { value: Some(Value::Float(produced)), .. }) =>
-                            Some((timestamp, (*consumed, *produced))),
-                        _ => Option::None
+                         TelegramContent { value: Some(Value::Float(produced)), .. }) => {
+                            if !processed_map.contains_key(&timestamp) {
+                                processed_map.insert(timestamp, (*consumed, *produced));
+                            } else {
+                                let old_value = processed_map.get(&timestamp).unwrap_or_else(|| bail!("Could not unpack old_value")).clone();
+                                processed_map.insert(timestamp, (old_value.0 + *consumed, old_value.1 + *produced));
+                            }
+                        }
+                        _ => ()
                     },
-                _ => Option::None
-            }
-        })
-        .collect();
+                _ => ()
+            };
+        });
 
-    let mut processed_timestamps: HashSet<UnixTimeStamp> = HashSet::new();
     let mut energy_pair_delta_over_time: EnergyOverTime = EnergyOverTime::new();
     let mut energy_pair_vector: Vec<EnergyData> = Vec::new();
+    let total_pairs: Vec<(UnixTimeStamp, (f64, f64))> = processed_map.into_iter().sorted_by_key(|p| p.0).collect();
     for (idx, (timestamp, (consumed, produced))) in total_pairs.iter().enumerate() {
         if idx == 0 {
-            continue;
-        }
-        if processed_timestamps.contains(timestamp) {
             continue;
         }
         energy_pair_vector.push(EnergyData {
@@ -204,7 +250,6 @@ fn main() -> Result<(), MainError> {
             consumed: *consumed - total_pairs[idx - 1].1.0,
             produced: *produced - total_pairs[idx - 1].1.1
         });
-        processed_timestamps.insert(*timestamp);
     }
     for _ in 0..12 {
         if let Some(e) = energy_pair_vector.pop() {
